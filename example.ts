972fb37b8fb7801c148c494d0044b74868c29e4a
Graeme Ross <graeme@oneutilitybill.co>
2018-03-23 11:46:00 +0000

    Updating example with hapi server

--- example.ts
+++ example.ts
@@ -1,4 +1,6 @@
 import * as graphqlBuilder from './index';
+import { Server } from 'hapi';
+import { graphqlHapi, graphiqlHapi } from 'graphql-server-hapi';
 
 const config: graphqlBuilder.IConfig = {
   path: `${__dirname}\\examples`,
@@ -7,3 +9,45 @@ const config: graphqlBuilder.IConfig = {
 };
 
 const schema = graphqlBuilder.creatExecutableSchema(config);
+
+const server = new Server({
+  port: 8080,
+  host: 'localhost',
+});
+
+const register = () => {
+  return server.register([{
+    plugin: graphqlHapi,
+    options: {
+      path: '/graphql',
+      graphqlOptions: (request) => ({
+        pretty: true,
+        schema,
+        context: request,
+      }),
+    },
+  }, {
+    plugin: graphiqlHapi,
+    options: {
+      path: '/graphiql',
+      graphiqlOptions: {
+        endpointURL: '/graphql',
+      },
+    },
+  }]);
+};
+
+const init = async () => {
+  await register();
+  await server.start();
+  // tslint:disable-next-line no-console
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  // tslint:disable-next-line no-console
+  console.log(err);
+  process.exit(1);
+});
+
+init();
